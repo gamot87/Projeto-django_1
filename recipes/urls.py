9113129534b1a6f1b8e860d7084776962f93b5f5
Gabriel Alencar <gamot1987@gmail.com>
2023-03-16 09:18:41 -0300

    v.4.4

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
+
+# o ponto abaixo significa : da pasta onde eu estou importe views
+
+
+# from recipes.views import home
+
 
 urlpatterns = [
-    path('', home),
-
-
+    path('', views.home),
+    # <id> é uma variável para indicar o valor que será inserido no caminho da
+    # url para identificar cada receita individualmente
+    # bem como é um parametro recebido pela função recipe
+    # int: -- só aceita inteiros
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,9 @@ def home(request):
     # home.html = arquivo html que o django a
     # utomaticamente irá procurar na pasta templates
     return render(request, 'recipes/pages/home.html', context={'name': 'Gabriel'})
+
+
+def recipe(request, id):
+    # recipe.html = arquivo html que o django a
+    # utomaticamente irá procurar na pasta templates
+    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Gabriel'})
